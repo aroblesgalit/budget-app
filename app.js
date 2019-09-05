4db50977f692514b90a9c7b84a14fe8b524176e6
Alvin Galit <aroblesgalit@gmail.com>
2019-09-04 23:55:11 -0500

    created a public method in the BUDGET CONTROLLER that returns the budget, total inc, total exp, and percentage, then called it inside the updateBudget function in the GLOBAL APP CONTROLLER, then updated the calculateBudget public method to only calculate the percentage when total inc is greater than 0

--- app.js
+++ app.js
@@ -75,7 +75,20 @@ var budgetController = (function() {
             data.budget = data.totals.inc - data.totals.exp;
 
             // Calculate the percentage of income that we spent
-            data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
 
         testing: function() {
@@ -183,10 +196,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     var updateBudget = function() {
 
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
+        console.log(budget);
 
     };
 
